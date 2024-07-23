bba30fa9adfa3cd1f0bb99f23fd8ce5faffc4016
Syed Jawad <jawadsyed406@gmail.com>
2024-07-23 19:59:08 +0300

    Building current weather component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Search from './components/search/search';
+import CurrentWeather from './components/current-weather/current-weather';
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
   return (
     <div className="container">
      <Search onSearchChange={handleOnSearchChange} />
+     <CurrentWeather />
     </div>
   );
 }
