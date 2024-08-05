a25c49197b1277827877c1033e82818761bb2358
Syed Jawad <jawadsyed406@gmail.com>
2024-08-05 23:05:41 +0300

    Fetching and mapping data from weather API

--- src/api.js
+++ src/api.js
@@ -1,31 +1,11 @@
-async function fetchData() {
-    const url = 'https://wft-geo-db.p.rapidapi.com/v1/geo/adminDivisions';
-    const options = {
-        method: 'GET',
+    export const geoApiOptions = {
+        method: "GET",
         headers: {
-            'X-RapidAPI-Key': 'b162e81b15mshc1a8ed7c24ee6d7p163124jsn41ad2bc1a5c5',
-            'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
-        }
-    };
+          "X-RapidAPI-Key": "b162e81b15mshc1a8ed7c24ee6d7p163124jsn41ad2bc1a5c5",
+          "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+        },
+      };
+      export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
-    try {
-        const response = await fetch(url, options);
-        if (!response.ok) {
-            if (response.status === 429) {
-                throw new Error('API Rate Limit Exceeded. Please try again later.');
-            } else {
-                throw new Error('Network response was not ok');
-            }
-        }
-        const contentType = response.headers.get('content-type');
-        if (contentType && contentType.includes('application/json')) {
-            const result = await response.json();
-            console.log(result);
-        } else {
-            const text = await response.text();
-            console.log('Non-JSON response:', text);
-        }
-    } catch (error) {
-        console.error('Error fetching data:', error);
-    }
-}
+      export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+      export const WEATHER_API_KEY = "dd7ad72aac8868eca8da340b8ba93a20";
