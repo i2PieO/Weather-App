32c386d87ce657152ae8175e3eae3b13e5dcc2fb
Syed Jawad <jawadsyed406@gmail.com>
2024-08-12 12:10:22 +0300

    Building weather forecast component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import './App.css';
-import Search from './components/search/search';
-import CurrentWeather from './components/current-weather/current-weather';
-import { WEATHER_API_URL, WEATHER_API_KEY } from './api';
-import { useState } from 'react';
+import { useState } from "react";
+import Search from "./components/search/search";
+import CurrentWeather from "./components/current-weather/current-weather";
+import Forecast from "./components/forecast/forecast";
+import { WEATHER_API_URL, WEATHER_API_KEY } from "./api";
+import "./App.css";
 
 function App() {
   const [currentWeather, setCurrentWeather] = useState(null);
@@ -11,27 +12,29 @@ function App() {
   const handleOnSearchChange = (searchData) => {
     const [lat, lon] = searchData.value.split(" ");
 
-    const currentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`);
-    const forecastFetch = fetch(`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`);
+    const currentWeatherFetch = fetch(
+      `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+    );
+    const forecastFetch = fetch(
+      `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+    );
 
     Promise.all([currentWeatherFetch, forecastFetch])
       .then(async (response) => {
         const weatherResponse = await response[0].json();
-        const forecastResponse = await response[1].json();
+        const forcastResponse = await response[1].json();
 
-        setCurrentWeather({ city: searchData.label , ...weatherResponse});
-        setForecast({ city: searchData.label, ...forecastResponse});
+        setCurrentWeather({ city: searchData.label, ...weatherResponse });
+        setForecast({ city: searchData.label, ...forcastResponse });
       })
-      .catch((err) => console.log(err));
-  }
-
-  console.log(currentWeather);
-  console.log(forecast);
+      .catch(console.log);
+  };
 
   return (
     <div className="container">
-     <Search onSearchChange={handleOnSearchChange} />
-     {currentWeather && <CurrentWeather data={currentWeather} />}
+      <Search onSearchChange={handleOnSearchChange} />
+      {currentWeather && <CurrentWeather data={currentWeather} />}
+      {forecast && <Forecast data={forecast} />}
     </div>
   );
 }
--- src/api.js
+++ src/api.js
@@ -1,10 +1,11 @@
-    export const geoApiOptions = {
-        method: "GET",
-        headers: {
-          "X-RapidAPI-Key": "b162e81b15mshc1a8ed7c24ee6d7p163124jsn41ad2bc1a5c5",
-          "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
-        },
-      };
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': 'b162e81b15mshc1a8ed7c24ee6d7p163124jsn41ad2bc1a5c5',
+		'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
       export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
       export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
